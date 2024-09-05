09ba1756f1458c0131d691134292262b8f936cd2
rahuljat1349 <rahuldudi1349@gmail.com>
2024-09-05 23:00:26 +0530

    update

---
+++ 02.Operators.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+// Data Types ...
+
+
+
+//Typecasting
+    float PI = 3.14;
+    int num = (int)PI;
+    std::cout << num << "\n";
+
+
+
+
+//Inputs
+
+    int age;
+    cout << "Enter the age : ";
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+
+
+
+
+// Operators
+
+    // Arithmatic
+    // + - * / % **
+
+
+    //Logical
+    // && || !
+
+
+
+    // Relational
+    // < <= > >= == !=
+
+
+
+    // Unary
+    // ++ --
+
+
+
+
+// Sum of two numbers ...
+
+    return 0;
+}
+
+
