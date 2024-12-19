c452a623f60941a7ee9d9af98660822f6af9b760
rahuljat1349 <rahuldudi1349@gmail.com>
2024-12-19 09:11:22 +0530

    Dec 19 challange

---
+++ 56.finalPrices.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// brut
+
+void getPrices(vector<int> &prices)
+{
+    for (int i = 0; i < prices.size(); i++)
+    {
+        for (int j = i + 1; j <= prices.size(); j++)
+        {
+            if (prices[j] <= prices[i])
+            {
+                prices[i] = prices[i] - prices[j];
+                break;
+            }
+        }
+    }
+}
+//
+//
+//
+//
+//
+//
+//
+//
+
+void getFinalPrices(vector<int> &prices)
+{
+    stack<int> s;
+
+    for (int i = 0; i < prices.size(); i++)
+    {
+        while (!s.empty() && prices[i] <= prices[s.top()])
+        {
+            prices[s.top()] = prices[s.top()] - prices[i];
+            s.pop();
+        }
+
+        s.push(i);
+    }
+}
+
+int main()
+{
+
+    vector<int> prices = {8, 4, 6, 2, 3};
+    getPrices(prices);
+
+    for (auto price : prices)
+    {
+        cout << price << " ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ 57.maxChunks.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int getMaxChunks(vector<int> &arr, vector<int> &currentChunk, vector<vector<int>> &ans)
+{
+    int maxVal = 0;
+    int maxChunks = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        maxVal = max(maxVal, arr[i]);
+        // currentChunk.push_back(arr[i]);
+
+        if (maxVal == i)
+        {
+            // ans.push_back(currentChunk);
+            // currentChunk.clear();
+            maxChunks++;
+        }
+    }
+    return maxChunks;
+}
+
+int main()
+{
+
+    vector<int> arr = {1, 0, 2, 3, 4};
+    vector<int> currentChunk;
+    vector<vector<int>> ans;
+
+    int result = getMaxChunks(arr, currentChunk, ans);
+    cout << result << endl;
+    
+    return 0;
+}
