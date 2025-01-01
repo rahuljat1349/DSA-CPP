4922238496daea98510d77d91a4e646fc11cd327
rahuljat1349 <rahuldudi1349@gmail.com>
2025-01-01 09:06:21 +0530

    add linked list and methods

--- 65.LinkedLists.cpp
+++ 65.LinkedLists.cpp
@@ -87,15 +87,85 @@ public:
         }
     }
 
-    void print(){
-        // search
+    void print()
+    {
+        if (head == NULL)
+        {
+            cout << "There is nothing to print!" << endl;
+            return;
+        }
+
+        Node *temp = head;
+
+        while (temp->next != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << temp->data << endl;
     }
-    //insert
-    //search
+
+    void insert(int data, int idx)
+    {
+        Node *temp = head;
+        if (idx < 0)
+        {
+            cout << "Invalid operation!" << endl;
+            return;
+        }
+
+        if (idx == 0)
+        {
+            push_front(data);
+            return;
+        }
+
+        for (int i = 0; i < idx - 1; i++)
+        {
+            temp  = temp->next;
+
+            if (temp == NULL)
+            {
+                cout << "Invalid operation!" << endl;
+                return;
+            }
+        }
+        Node *newNode = new Node(data);
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+    
+    int search(int val){
+        Node* temp = head;
+        int idx = 0;
+        while (temp != NULL )
+        {
+           if(temp->data == val) {
+            return idx;
+           }
+           idx++;
+           temp= temp->next;
+        }
+        return -1;
+    }
 };
 
 int main()
 {
+    List ll;
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_front(4);
+    ll.push_front(5);
 
+    // ll.insert(7, -1);
+
+    cout << ll.search(3) << endl;;
+
+
+
+
+    ll.print();
     return 0;
 }
