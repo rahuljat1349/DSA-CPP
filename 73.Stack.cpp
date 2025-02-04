c7119fc14abc813bafee4ea0ca37e8b3f5a25ae6
rahuljat1349 <rahuldudi1349@gmail.com>
2025-02-04 10:27:38 +0530

    stack using linked list

--- 73.Stack.cpp
+++ 73.Stack.cpp
@@ -1,12 +1,35 @@
 #include<iostream>
+#include<list>
+#include<vector>
+#include<stack>
 using namespace std;
+
+
+// using linked list
+class stackList{
+    list<int> l;
+public:
+    void push(int x){
+        l.push_front(x);
+    }
+    void pop(){
+        l.pop_front();
+    }
+    int top(){
+        return l.front();
+    }
+    bool empty(){
+        return l.size()==0;
+    }
+};
+
 
 // using vector
 class Stack {
     vector<int> v;
 public:
-    void push(int val){
-        v.push_back(val);
+    void push(int x){
+        v.push_back(x);
     }
     void pop(){
         v.pop_back();
@@ -15,7 +38,7 @@ public:
         return v[v.size()-1];
     }
     bool empty(){
-        return v.size() == 0;
+        return v.size()==0;
     }
 };
 
@@ -25,11 +48,15 @@ int main()
     s.push(1);
     s.push(2);
     s.push(3);
-    while(!s.empty()){
-        cout<<s.top()<<endl;
-        s.pop();
-    }
-    cout<<endl;
+    cout<<s.top()<<endl;
+    
+
+    stackList sl;
+    sl.push(1);
+    sl.push(2);
+    sl.push(3);
+    cout<<sl.top()<<endl;
+    
     
     return 0;
 }
