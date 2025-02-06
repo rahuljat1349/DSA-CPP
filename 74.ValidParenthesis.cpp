875305b1ceb919992b23211f0072bae2698ae208
rahuljat1349 <rahuldudi1349@gmail.com>
2025-02-06 10:47:47 +0530

    add valid parenthesis

---
+++ 74.ValidParenthesis.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+ bool isValid(string str) {
+        stack<char> s;
+
+        for (int i = 0; i < str.size(); i++) {
+
+            if (str[i] == '(' || str[i] == '[' || str[i] == '{') {
+                s.push(str[i]);
+            }
+
+            else {
+
+                if (s.size() == 0) {
+                    return false;
+                }
+
+                if (s.top() == '(' && str[i] == ')' ||
+                    s.top() == '[' && str[i] == ']' ||
+                    s.top() == '{' && str[i] == '}') {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return s.size() == 0;
+    }
+int main()
+{
+    int str = "({[]})";
+    cout<<isValid(str)<<endl;
+    return 0;
+}
