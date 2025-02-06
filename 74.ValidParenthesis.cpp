1c2f7660792219b553df502fcdf36cb94b198e98
rahuljat1349 <rahuldudi1349@gmail.com>
2025-02-06 11:03:11 +0530

    add valis parenthesis

--- 74.ValidParenthesis.cpp
+++ 74.ValidParenthesis.cpp
@@ -1,35 +1,37 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 
- bool isValid(string str) {
-        stack<char> s;
 
-        for (int i = 0; i < str.size(); i++) {
-
-            if (str[i] == '(' || str[i] == '[' || str[i] == '{') {
-                s.push(str[i]);
-            }
-
-            else {
-
-                if (s.size() == 0) {
-                    return false;
-                }
-
-                if (s.top() == '(' && str[i] == ')' ||
-                    s.top() == '[' && str[i] == ']' ||
-                    s.top() == '{' && str[i] == '}') {
-                    s.pop();
-                } else {
-                    return false;
-                }
-            }
+bool isValid(int str){
+    stack<char> s;
+    for(int i = 0; i<str.size(); i++){
+        if(str[i] == '(' || str[i] == '[' || str[i] == '{'){
+            s.push(str[i]);
         }
-        return s.size() == 0;
+        else{
+            if(s.size() == 0){
+                return false;
+            }
+
+            if(s.top() == '(' && str[i] == ')' ||
+               s.top() == '[' && str[i] == ']' ||
+               s.top() == '{' && str[i] == ']'){
+                s.pop();
+               }
+               else{
+                return false;
+               }
+        }
     }
-int main()
-{
+    return s.size() == 0;
+}
+
+int main(){
     int str = "({[]})";
-    cout<<isValid(str)<<endl;
+
+    cout << isValid(str) << endl;
+
+
     return 0;
 }
